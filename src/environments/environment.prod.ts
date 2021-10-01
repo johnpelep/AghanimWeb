48efd5f812c414d9dede8b9d68aab5d0ac43cf9f
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2021-10-01 21:24:28 +0800

    added open_dota_url to environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   aghanim_api_url:
     'https://api.allorigins.win/raw?url=http://ec2-3-1-83-97.ap-southeast-1.compute.amazonaws.com/api/players',
+  open_dota_url: 'https://api.opendota.com',
 };
