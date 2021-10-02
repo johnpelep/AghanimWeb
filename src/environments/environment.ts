081e7d73b1c45270f330dbdcdecc8d70d9efaf21
John Philip Cesar P. Morales <johnphilipcesarmorales@gmail.com>
2021-10-03 01:17:01 +0800

    show past 30 days in chart

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  aghanim_api_url:
-    'http://ec2-3-1-83-97.ap-southeast-1.compute.amazonaws.com/api/players',
+  aghanim_api_url: 'https://localhost:5001',
   open_dota_url: 'https://api.opendota.com',
 };
 
